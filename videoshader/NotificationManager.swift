c481f64e70b4827e645c2c149a610badd28365d5
Arturo <me@arturojamaica.com>
2018-04-25 16:52:28 +0900

    Fix: Support for iPhone X and iOS 11

--- videoshader/NotificationManager.swift
+++ videoshader/NotificationManager.swift
@@ -13,12 +13,12 @@ class NotificationManager {
     
     deinit {
         for observer in observers {
-            NSNotificationCenter.defaultCenter().removeObserver(observer)
+            NotificationCenter.default.removeObserver(observer)
         }
     }
     
-    func addObserverForName(name: String?, object obj: AnyObject?, queue: NSOperationQueue?, usingBlock block: (NSNotification!) -> Void) {
-        let observer = NSNotificationCenter.defaultCenter().addObserverForName(name, object: obj, queue: queue, usingBlock: block)
+    func addObserverForName(name: String?, object obj: AnyObject?, queue: OperationQueue?, usingBlock block: @escaping (Notification??) -> Void) {
+        let observer =  NotificationCenter.default.addObserver(forName: name.map { NSNotification.Name(rawValue: $0) }, object: obj, queue: queue, using: block)
         observers.append(observer)
     }
 }
